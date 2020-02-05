4d1d992408bac1fb224a1d030e166a4012e48dcb
Ubuntu <ubuntu@ip-172-31-42-70.ec2.internal>
2020-02-05 08:31:59 +0000

    updated database url

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -83,11 +83,9 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': dj_database_url.parse(os.environ["DATABASE_URL"])
     }
-}
+
 
 
 
