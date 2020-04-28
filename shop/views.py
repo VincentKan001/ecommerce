a1aa8501ed655f3bf53f672029278e23a4846602
Ubuntu <ubuntu@ip-172-31-42-70.ec2.internal>
2020-04-28 08:55:52 +0000

    updated static link

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from .forms import NewProduct
+
 
 # Create your views here.
 def catalog(request):
@@ -7,3 +9,10 @@ def catalog(request):
     return render(request, 'shop/catalog.html',{
         'all_products':all_products
     })
+    
+def add_product(request):
+    entry_form = NewProduct()
+    return render(request, 'shop/add_product.html',{
+        'form' : entry_form
+    })
+    # return render(request, 'shop/add_product.html')
